32452ce271d12e210a9b1ee9d7a7287b33f28365
Cory <Coryf65@live.com>
2018-10-24 19:23:20 -0500

    Commented out Objects Practice Region, Starting Array Practice Region

--- Program.cs
+++ Program.cs
@@ -10,23 +10,37 @@ namespace C_Sharp_PracticeSessions
     {
         static void Main(string[] args)
         {
-     
-            Console.WriteLine("C# Practice..." + "\r\n");
 
-            var album = new Album("Disease", "Beartooth");           
-            var book = new Book("Don't Make Me Think", "Steve Krug");   
-            var movie = new Movie("Lego Movie", "Chris Miller, Phil Lord");
+            #region "Part of Objects Practice"
+
+            //Console.WriteLine("C# Practice..." + "\r\n");
+
+            //var album = new Album("Disease", "Beartooth");           
+            //var book = new Book("Don't Make Me Think", "Steve Krug");   
+            //var movie = new Movie("Lego Movie", "Chris Miller, Phil Lord");
 
 
-            Console.WriteLine("Album: " + album.Title + " by, " + album.Artist);
-            Console.WriteLine("Book: " + book.Title + " by, " + book.Author);
-            Console.WriteLine("Movie: " + movie.Title + " by, " + movie.Director);
+            //Console.WriteLine("Album: " + album.Title + " by, " + album.Artist);
+            //Console.WriteLine("Book: " + book.Title + " by, " + book.Author);
+            //Console.WriteLine("Movie: " + movie.Title + " by, " + movie.Director);
 
-            // showing that we can change the fields after
-            album.Title = "Sick";
+            //// showing that we can change the fields after
+            //album.Title = "Sick";
 
-            // and now it changed viola!
-            Console.WriteLine("Album: " + album.Title + " by, " + album.Artist);
+            //// and now it changed viola!
+            //Console.WriteLine("Album: " + album.Title + " by, " + album.Artist);
+
+            #endregion
+
+
+        #region "Part of Arrays Practice"
+            
+            // arrays 
+            int[] lockcombination = new int[3];
+
+            Console.WriteLine(lockcombination);
+        #endregion
+
 
         }
     }

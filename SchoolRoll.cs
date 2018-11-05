afd25ae02d972f8f2af7f626ccf061466e451480
Cory <Coryf65@gmail.com>
2018-11-05 11:59:53 -0600

    Changed the AddStudents to an IEnumerable type, if they passed in an array they could now use this.

--- SchoolRoll.cs
+++ SchoolRoll.cs
@@ -10,7 +10,17 @@ namespace C_Sharp_PracticeSessions
     {
         private List<Student> _students = new List<Student>();
 
-        public void AddStudents(List<Student> students)
+        // But what if they had an array and wanted to pass that in
+        // Then they would have to convert to a list
+
+            // but we only need to add those into a list, we can change this to an IEnumerable.
+
+        /// <summary>
+        /// There is a method named AddStudents that takes a list of students and
+        /// then adds all of the new students to the list of students already in the school.
+        /// </summary>
+        /// <param name="students"> Pass as a List Type</param>
+        public void AddStudents(IEnumerable<Student> students)
         {
             _students.AddRange(students);
         }

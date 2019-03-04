ace5228e132026cec0a29ae06c79f810ec29afec
Cory <Coryf65@gmail.com>
2019-03-03 18:45:53 -0600

    Set up the Album class

--- Album.cs
+++ Album.cs
@@ -6,18 +6,34 @@ using System.Threading.Tasks;
 
 namespace C_Sharp_PracticeSessions
 {
-    class Album
+    class Album : MediaType
     {
+        //field
+        public readonly string Artist;
 
-        // fields
-        public string Title;
-        public string Artist;
-
-        // constructor
         public Album(string title, string artist)
+            : base(title)
         {
-            Title = title;
             Artist = artist;
+        }
+
+        public string GetDisplayText()
+        {
+            string text = "Album: " + Title + " by " + Artist;
+
+            if (OnLoan)
+            {
+                if (!string.IsNullOrEmpty(Loanee))
+                {
+                    text += " (Currently on loan to " + Loanee + ")";
+                }
+                else
+                {
+                    text += " (Currently on loan)";
+                }
+            }
+
+            return text;
         }
 
     }
